b6556eb663436613b571ead217b62deaa9c29710
fuadsaniibrahim <fuadsani74@gmail.com>
2024-03-10 14:26:09 +0530

    HEADER.JS CREATED

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className='Header'>
+      <h1>I am a header</h1>
+      
+
+
+
+    </div>
+  )
+}
+
+export default Header
